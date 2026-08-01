6bc9f5ced5a0a99452f8473f12f4458a7885be36
Gabriel <gabrielvillacordero@gmail.com>
2026-07-31 20:22:16 -0400

    Implementa funcionalidad de lectura de base de datos

--- crud.py
+++ crud.py
@@ -5,5 +5,8 @@ def mostrar_menu():
     print("\n--- Sistema CRUD ---")
     print("1. Crear | 2. Leer | 3. Actualizar | 4. Eliminar | 5. Salir")
 
+def leer():
+    print("Registros actuales:", db)
+
 if __name__ == "__main__":
     mostrar_menu()
