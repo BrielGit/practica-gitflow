4eaf0e1ef0e61ed3813a721b6d177d120a7a2406
Gabriel <gabrielvillacordero@gmail.com>
2026-07-31 20:25:06 -0400

    Implementa funcionalidad de borrado

--- crud.py
+++ crud.py
@@ -5,5 +5,12 @@ def mostrar_menu():
     print("\n--- Sistema CRUD ---")
     print("1. Crear | 2. Leer | 3. Actualizar | 4. Eliminar | 5. Salir")
 
+def eliminar(id):
+    if id in db:
+        del db[id]
+        print(f"Eliminado: {id}")
+    else:
+        print("ID no encontrado.")
+        
 if __name__ == "__main__":
     mostrar_menu()
